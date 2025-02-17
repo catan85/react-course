0e0c084caf424772833d92a3793f455f069ab550
CAMOZZIGROUP\ancattaneo <ancattaneo@camozzi.com>
2025-02-17 20:47:21 +0100

    added tic tac toe game

---
+++ 07-tic-tac-toe/src/App.jsx
@@ -0,0 +1,32 @@
+
+import Player from './components/Player.jsx'
+import GameBoard from './components/GameBoard.jsx'
+import {useState} from 'react'
+
+function App() {
+
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare(){
+    console.log("selected square");
+
+    setActivePlayer((curActivePlayer) => {
+      console.log(curActivePlayer)
+      return curActivePlayer === 'X' ? 'O' : 'X' 
+    } );
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className='highlight-player'>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ 07-tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+
+export default function Player({ initialName, symbol, isActive}){
+
+    const [ isEditing, setIsEditing ] = useState(false);
+    const [ playerName, setPlayerName ] = useState(initialName);
+
+
+    function handleEditClick(){
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleInputEdit(event){
+        setPlayerName((name)=>event.target.value);
+    }
+
+    let playerNameControl = <span className="player-name">{playerName}</span>;
+    if (isEditing){
+        playerNameControl = <input type="text" required value={playerName} onChange={handleInputEdit}></input>
+    }
+
+    return (
+        <li className={ isActive ? 'active' : undefined } >
+        <span className="player">
+          {playerNameControl}
+          <span className="player-symbol">{symbol}</span>
+          <button onClick={handleEditClick}>{ isEditing ? "Save" : "Edit" }</button>
+        </span>
+      </li>
+    );
+}
